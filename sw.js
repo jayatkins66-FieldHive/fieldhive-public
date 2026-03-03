e186824b8e3d5e0b506ddf3d52cb828518b49e6e
jayatkins66-FieldHive <jayatkins66@gmail.com>
2026-03-02 19:48:23 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,8 +2,9 @@ self.addEventListener("push", function (event) {
   const data = event.data ? event.data.json() : {};
 
   const title = data.title || "FieldHive Alert";
+
   const options = {
-    body: data.message || "New civic alert",
+    body: data.body || data.message || "New civic alert",
     icon: "/logo.svg",
     badge: "/logo.svg",
   };
